69e9052d186f439a167296a06bc619334708ca71
schochtlts <102829193+schochtlts@users.noreply.github.com>
2024-05-09 18:20:16 +0200

    Environnement preparation
    title

--- README.md
+++
@@ -1,2 +0,0 @@
-# geodx
-Territorial.io inspired strategy game written in C to WebAssembly.
---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE int main(){
+    printf("cc encore nouveau!\n");
+
+    return 0;
+}
