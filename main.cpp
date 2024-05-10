18f079a1d7204c27491434ad2d3b8def80c86547
schochtlts <102829193+schochtlts@users.noreply.github.com>
2024-05-10 10:21:59 +0200

    Basic stuff
    title

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 
-#include <stdio.h>
-#include <emscripten.h>
+#include <SDL2/SDL.h>
 
-EMSCRIPTEN_KEEPALIVE int main(){
-    printf("cc encore nouveau!\n");
+int main(){
+  SDL_Init(SDL_INIT_VIDEO);
 
-    return 0;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_CreateWindowAndRenderer(300, 300, 0, &window, &renderer);
+
+  SDL_SetRenderDrawColor(renderer, /* RGBA: green */ 0x00, 0x80, 0x00, 0xFF);
+  SDL_Rect rect = {.x = 10, .y = 10, .w = 150, .h = 100};
+  SDL_RenderFillRect(renderer, &rect);
+
+  SDL_RenderPresent(renderer);
 }
