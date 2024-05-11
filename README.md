29f9624be50f41acc56b3a836001aff13d8dcb7b
schochtlts <102829193+schochtlts@users.noreply.github.com>
2024-05-11 23:19:49 +0200

    nouveauté
    titre

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# geodx
+
+Territorial strategy game heavly inspired by [territoial.io](https://territorial.io).
+
+CURRENTLY IN DEVELOPMENT
--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 
 #include <SDL2/SDL.h>
+#include <emscripten.h>
 
 int main(){
   SDL_Init(SDL_INIT_VIDEO);
-
   SDL_Window *window;
   SDL_Renderer *renderer;
-  SDL_CreateWindowAndRenderer(300, 300, 0, &window, &renderer);
+  int geodx_WindowWidth = EM_ASM_INT(return window.innerWidth);
+  int geodx_WindowHeight = EM_ASM_INT(return window.innerHeight);
+  SDL_CreateWindowAndRenderer(geodx_WindowWidth, geodx_WindowHeight, 0, &window, &renderer);
 
-  SDL_SetRenderDrawColor(renderer, /* RGBA: green */ 0x00, 0x80, 0x00, 0xFF);
+  SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
   SDL_Rect rect = {.x = 10, .y = 10, .w = 150, .h = 100};
   SDL_RenderFillRect(renderer, &rect);
 
   SDL_RenderPresent(renderer);
+
+  return 0;
 }
