aa4456e2c3185c2d84a76cf32e7db76c8bed992d
schochtlts <102829193+schochtlts@users.noreply.github.com>
2024-05-29 15:04:03 +0200

    j'ai pas compris les stashs

--- src/engine/linalg.hpp
+++ src/engine/linalg.hpp
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include <cstdint>
@@ -9,86 +10,6 @@
 class Vec3
 {
 public:
-  double x;
-  double y;
-  double z;
-
-  Vec3(double X = 0, double Y = 0, double Z = 0) {
-    x = X;
-    y = Y;
-    z = Z;
-  }
-
-  ~Vec3() {}
-
-  double norm() const;
-  Vec3 normalized() const ;
-  Vec3 rotated_x(double) const;
-  Vec3 rotated_y(double) const;
-  Vec3 rotated_z(double) const;
-};
-
-double Vec3::norm() const {
-  return std::sqrt(x*x + y*y + z*z);
-}
-
-Vec3 Vec3::normalized() const {
-  const double n = norm();
-  return Vec3(x/n, y/n, z/n);
-}
-
-Vec3 Vec3::rotated_x(double angle) const {
-  const double cos = std::cos(angle), sin = std::sin(angle);
-  Vec3 C;
-  C.x = x;
-  C.y = cos*y - sin*z;
-  C.z = sin*y + cos*z;
-  return C;
-}
-
-Vec3 Vec3::rotated_y(double angle) const {
-  const double cos = std::cos(angle), sin = std::sin(angle);
-  Vec3 C;
-  C.x = cos*x + sin*z;
-  C.y = y;
-  C.z = cos*z - sin*x;
-  return C;
-}
-
-Vec3 Vec3::rotated_z(double angle) const {
-  const double cos = std::cos(angle), sin = std::sin(angle);
-  Vec3 C;
-  C.x = cos*x - sin*y;
-  C.y = sin*x + cos*y;
-  C.z = z;
-  return C;
-}
-
-Vec3 operator+(Vec3 A, Vec3 B) {
-  return Vec3(A.x + B.x, A.y + B.y, A.z + B.z);
-}
-
-Vec3 operator-(Vec3 A, Vec3 B) {
-  return Vec3(A.x - B.x, A.y - B.y, A.z - B.z);
-}
-
-Vec3 operator*(double s, Vec3 v) {
-  return Vec3(s*v.x, s*v.y, s*v.z);
-}
-
-Vec3 middle(Vec3 A, Vec3 B) {
-  Vec3 C;
-  C.x = (A.x + B.x)/2.0f;
-  C.y = (A.y + B.y)/2.0f;
-  C.z = (A.z + B.z)/2.0f;
-  return C;
-}
-
-double dot(Vec3 A, Vec3 B) {
-  return A.x*B.x + A.y*B.y + A.z*B.z;
-}
-
-class Mat3x4
 {
 public:
   double mat[3][4];
@@ -159,4 +80,10 @@ Mat3x4 axis_angle_to_mat(Vec3 axis, double angle) {
                             { -axis.y, axis.x, 0.0f, 0.0f } });
 
   return IDENT_MAT3x4 + sin*K + (1.0f - cos)*(K*K);
+}
+
+void Vec3::setPosFromArray(double pos[3]){
+  _x=pos[0];
+  _y=pos[1];
+  _z=pos[2];
 }
